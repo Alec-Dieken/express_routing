fcd6f0784232219024eb6f13fa017a19670627d9
ajdieken <ajdieken@live.com>
2022-12-13 09:00:31 -0800

    added exercise files

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    return res.send('Hello World');
+});
+
+app.get('/mean', (req, res) => {
+    let nums = req.query.nums.split(",");
+    let mean = 0;
+    for(let n of nums) {
+        mean += parseInt(n);
+    }
+    return res.json({response: 
+                      {operation: "mean",
+                       value: mean / nums.length}});
+});
+
+app.get('/median', (req, res) => {
+    let nums = req.query.nums.split(",").sort((a, b) => a - b).map(v => parseInt(v));
+    let median = nums.length % 2 === 1 
+        ? nums[Math.floor(nums.length / 2)] 
+        : (nums[(nums.length / 2) - 1] + nums[(nums.length / 2)]) / 2 ;
+    
+    return res.json({response: 
+                      {operation: "median",
+                       value: median}});
+});
+
+app.get('/mode', (req, res) => {
+    let nums = req.query.nums.split(",").sort((a, b) => a - b).map(v => parseInt(v));
+    let count = 0;
+    let max = 0;
+    let mode = undefined;
+
+    for(let i = 0; i < nums.length; i++) {
+        count += 1;
+        if(count > max) {
+            max = count;
+            mode = nums[i];
+        }
+        if(nums[i+1] && nums[i] != nums[i+1]) {
+            count = 0;
+        }
+    }
+    
+    return res.json({response: 
+                      {operation: "mode",
+                       value: mode}});
+});
+
+app.listen(3000, () => {
+    console.log('App on port 3000');
+});
